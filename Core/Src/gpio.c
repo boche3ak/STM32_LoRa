c6bc636cfd88d7a8409998762510600ae1b3ccaf
Oleksandr Kotenkov <ok@aleko-embedded.com>
2026-05-27 19:16:36 +0200

    gpio fixes - inverted PC13 inclusion - PA8 init as input added

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -87,7 +87,7 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-#ifndef WATCHDOG_ENABLED
+#ifdef WATCHDOG_ENABLED
   __HAL_RCC_GPIOC_CLK_ENABLE();
     /*Configure GPIO pin : PC13/USER LED */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
@@ -96,6 +96,13 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 #endif
+
+  /*Configure GPIO pin : PA8 (Challenger/Transponder) */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 2 */
