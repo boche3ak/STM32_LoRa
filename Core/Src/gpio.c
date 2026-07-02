e5325670586da5b173402170623491d69fa975d2
Oleksandr Kotenkov <ok@aleko-embedded.com>
2026-07-02 18:19:49 +0200

    LED not working fix

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -87,7 +87,7 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-#ifdef WATCHDOG_ENABLED
+#ifndef WATCHDOG_ENABLED
   __HAL_RCC_GPIOC_CLK_ENABLE();
     /*Configure GPIO pin : PC13/USER LED */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
@@ -95,6 +95,7 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
 #endif
 
   /*Configure GPIO pin : PA8 (Challenger/Transponder) */
