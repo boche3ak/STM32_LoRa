07c7852c3573895e9393bd971424e4f46ab68023
Oleksandr <ok@aleko-embedded.com>
2026-07-31 17:31:08 +0200

    fix of missing PA3 init

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -63,7 +63,7 @@ enum {
 /* pin definitions according to the current schematics*/
 #define PIN_READ_WHOAMI                  HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8)           /* PA8 physical switch to select Challenger or Receiver*/
 #define PIN_WRITE_STAT_FRIEND_FOF(state) HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, (state)) /* PA2 external LED, FRIEND/FOE indication, e.g. in front of the POV cam*/
-#define PIN_WRITE_STAT_POWERON(state)    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, (state)) /* PA3 external LED, POWERON indication*/
+#define PIN_WRITE_STAT_POWERON(state)    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, (state)) /* PA3 external LED, POWER-ON indication*/
 #define PIN_WRITE_STAT_HEARTBEAT(state)  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, (state))/* PC13 - standard bluepill available LED ->only for debug purposes */
 #define STAT_FRIEND                      GPIO_PIN_SET
 #define STAT_FOE                         GPIO_PIN_RESET
